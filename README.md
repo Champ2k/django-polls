fea7465f304ede78fe3ed8d9ffb43c5602115cc2
Wikrom Chanthakhun <you@example.com>
2019-09-16 15:53:18 +0700

    edit Readme add Requirements, hide the confidential data and .env file

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Django-polls
-by Wikrom Chanthakhun
+by *Wikrom Chanthakhun*
+
+---
+
+ ## Django Polls Application
+### Description
+ This is the creation of a basic poll application by using Django.
+
+## Requirements
+
+ The application requires
+ * Python 3.6 or newer
+ * Django 2.1.2 or newer
+ * Python add-on modules as in [requirements.txt](requirements.txt)
+
+ ## How to Run
+First check all the requirements.
+<br>
+Run ***python manage.py runserver*** in the terminal.
+<br>
+Go to link ***http:/local_host:port/polls***.
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7%iy&3yxh8$1r4h)5qxy6iof7v*(=)9_l5tn*3tltkp4+o&f0i'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
