442799d6a80a0e24b9b04fad37dcdc8d92cb9049
Wikrom Chanthakhun <you@example.com>
2019-09-25 09:58:25 +0700

    set default of confidential data

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', cast=bool, default='Fasle')
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=list)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=list ,default=[])
 
 
 # Application definition
